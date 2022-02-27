08314b621afbc8e61f91a26a4eac3a4d6d902667
leonardonadi@gmail.com <Dist@1#2>
2022-02-27 08:01:51 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,37 @@
+var welcome = "Hello Javascript - Automação Full Stack from QA Ninja via arquivo"
+document.getElementById("welcome").append(welcome)
+console.log(welcome)
+
+//Operadores Aritiméticos
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(8/4);
+
+//Comparação
+console.log(2==2);
+console.log(2==3);
+
+//Não igual
+console.log(2!=2);
+console.log(2!=1);
+
+//Igual estrito
+console.log(2=="2") //compara o tipo
+console.log(2==="2") //compara o valor e o tipo
+
+//Não igual estrito
+console.log(3 != "3")
+console.log("3" != "3")
+
+//Maior que >
+console.log(2>3);
+console.log(2>1);
+
+//Maior que <
+//Maior igual >=
+console.log(2>=3);
+console.log(2>=2);
+console.log(2>=1);
+
+
---
+++ variaveis.js
@@ -0,0 +1,14 @@
+//Varáveis no Javascript
+var number = 10;
+console.log(number);
+
+//Exibir tipo da variável
+console.log(typeof number);
+
+var name = "Leandro";
+
+console.log(typeof name);
+
+var n1 = 10;
+var n2 = 20;
+console.log(n1+n2);
